e51078f279cb68283cb9aa4473bf6236d0fb58a5
Bhavani <bhavanindrnf@gmail.com>
2022-07-05 11:26:25 +0530

    projrcttilljuly5

--- src/components/Checkbalance.js
+++ src/components/Checkbalance.js
@@ -19,62 +19,73 @@ const columns = [
     {
         title: 'Email',
         dataIndex: 'Email',
-        key: 'Email', 
+        key: 'Email',
     },
     {
         title: 'Phone',
         dataIndex: 'Phone',
-        key: 'Phone', 
+        key: 'Phone',
     },
     {
         title: 'Balance',
         dataIndex: 'Balance',
-        key: 'Balance', 
+        key: 'Balance',
     },
-    
+
 ]
 
 const data = [
-   { 
-    key: '0',
-    Email: 'dd0@dd.com',
-    Phone: '999912339',
-    Balance: 1000,
-   
-  },
-  { 
-    key: '1',
-    Email: 'dd1@dd.com',
-    Phone: '989912339',
-    Balance: 2000,
-   
-  },
-  { 
-    key: '2',
-    Email: 'dd2@dd.com',
-    Phone: '97712339',
-    Balance: 3000,
-   
-  },
+    {
+        key: '0',
+        Email: 'dd0@dd.com',
+        Phone: '999912339',
+        Balance: 1000,
+
+    },
+    {
+        key: '1',
+        Email: 'dd1@dd.com',
+        Phone: '989912339',
+        Balance: 2000,
+
+    },
+    {
+        key: '2',
+        Email: 'dd2@dd.com',
+        Phone: '97712339',
+        Balance: 3000,
+
+    },
 ]
 
 
 function Balance() {
     return (
         <div>
+        <div>
+        <Navigation status={true} />
+        </div>
+        <div>
+        
         <Row type="flex" align="top" justify="start">
             <Col xs ={24} sm={24} md={6} lg={6} xl={6}>
                 <SideNavbar />
             </Col>
             <Col xs ={24} sm={24} md={18} lg={18} xl={18}>
+            <h1><center>Details</center></h1>
             <Table
                     dataSource={data}
                     columns={columns}
                     />
             </Col>
         </Row></div>
-
         
+            <div className="footerBottom">
+        <Footer />
+      </div>
+      </div>
+
+
         // <div className="createUser">
 
         //     <div>
@@ -86,62 +97,62 @@ function Balance() {
         //         <SideNavbar />
         //     </div>
         //     {/* <Row type="flex" align="middle" justify="center">
-            //     <Col xs ={24} sm={24} md={24} lg={24} xl={24}>
-            //         <Card>
+        //     <Col xs ={24} sm={24} md={24} lg={24} xl={24}>
+        //         <Card>
 
-            //         </Card>
-            //     </Col>
-            // </Row>
-            // <div className="userForm">
-            //     <Form
-            //         className="userMainForm"
-            //         labelCol={{ span: 4 }}
-            //         wrapperCol={{ span: 14 }}
-            //         layout="horizontal"
-            //     >
-            //       <div className="form-inline C_cx" >
-            //       <h2><center><font color="white">Balance</font></center></h2>
-            //  </div> 
-            //         <Form.Item label="Email">
-            //             <Descriptions.Item label="Email">abc@zaggle.in</Descriptions.Item>
-            //         </Form.Item>
-            //         <Form.Item label="Mobile">
-            //             <Descriptions.Item label="Mobile">1810000000</Descriptions.Item>
-            //         </Form.Item>
-            //         <Form.Item label="Balance">
-            //             <Descriptions.Item label="Balance">100000</Descriptions.Item>
-            //         </Form.Item>
-
-                
-
-                    /* <table class="ui celled table">
-                        <thead>
-                            <tr><th>Name</th>
-                                <th>Age</th>
-                                <th>Balance</th>
-                            </tr></thead>
-                        <tbody>
-                            <tr>
-                                <td data-label="Name">James</td>
-                                <td data-label="Age">24</td>
-                                <td data-label="Balance">100000</td>
-                            </tr>
-
-                        </tbody>
-                    </table> */
-                /* </Form>
-                <Table
-                    dataSource={data}
-                    columns={columns}
-                    />
-
-            </div>
+        //         </Card>
+        //     </Col>
+        // </Row>
+        // <div className="userForm">
+        //     <Form
+        //         className="userMainForm"
+        //         labelCol={{ span: 4 }}
+        //         wrapperCol={{ span: 14 }}
+        //         layout="horizontal"
+        //     >
+        //       <div className="form-inline C_cx" >
+        //       <h2><center><font color="white">Balance</font></center></h2>
+        //  </div> 
+        //         <Form.Item label="Email">
+        //             <Descriptions.Item label="Email">abc@zaggle.in</Descriptions.Item>
+        //         </Form.Item>
+        //         <Form.Item label="Mobile">
+        //             <Descriptions.Item label="Mobile">1810000000</Descriptions.Item>
+        //         </Form.Item>
+        //         <Form.Item label="Balance">
+        //             <Descriptions.Item label="Balance">100000</Descriptions.Item>
+        //         </Form.Item>
 
 
-            <div>
-                <Footer />
-            </div>
-        </div> */
+
+        /* <table class="ui celled table">
+            <thead>
+                <tr><th>Name</th>
+                    <th>Age</th>
+                    <th>Balance</th>
+                </tr></thead>
+            <tbody>
+                <tr>
+                    <td data-label="Name">James</td>
+                    <td data-label="Age">24</td>
+                    <td data-label="Balance">100000</td>
+                </tr>
+
+            </tbody>
+        </table> */
+        /* </Form>
+        <Table
+            dataSource={data}
+            columns={columns}
+            />
+
+    </div>
+
+
+    <div>
+        <Footer />
+    </div>
+</div> */
     );
 }
 
--- src/components/Viewuser.js
+++ src/components/Viewuser.js
@@ -19,51 +19,51 @@ const columns = [
     {
         title: 'UserName',
         dataIndex: 'UserName',
-        key: 'UserName', 
+        key: 'UserName',
     },
     {
         title: 'Telephone',
         dataIndex: 'Telephone',
-        key: 'Telephone', 
+        key: 'Telephone',
     },
     {
         title: 'Live',
         dataIndex: 'Live',
-        key: 'Live', 
+        key: 'Live',
     },
     {
         title: 'Address',
         dataIndex: 'Address',
-        key: 'Address', 
+        key: 'Address',
     },
-    
+
 ]
 
 const data = [
-   { 
-    key: '0',
-    UserName: 'dd0@dd.com',
-    Telephone: '999912339',
-    Live:'Live',
-    Address: 1000,
-   
-  },
-  { 
-    key: '1',
-    UserName: 'dd0@dd.com',
-    Telephone: '999912339',
-    Live:'Live',
-    Address: 1000,
-   
-  },
-  { 
-    key: '2',
-    UserName: 'dd0@dd.com',
-    Telephone: '999912339',
-    Live:'Live',
-    Address: 1000,
-   
-  },
+    {
+        key: '0',
+        UserName: 'dd0@dd.com',
+        Telephone: '999912339',
+        Live: 'Live',
+        Address: 1000,
+
+    },
+    {
+        key: '1',
+        UserName: 'dd0@dd.com',
+        Telephone: '999912339',
+        Live: 'Live',
+        Address: 1000,
+
+    },
+    {
+        key: '2',
+        UserName: 'dd0@dd.com',
+        Telephone: '999912339',
+        Live: 'Live',
+        Address: 1000,
+
+    },
 ]
 
 
@@ -74,45 +74,32 @@ function Fetchbalance() {
             <div>
                 <Navigation status={true} />
             </div>
-            <Row type="flex" align="top" justify="start">
-                <Col xs={24} sm={24} md={24} lg={24} xl={24}>
-                   
+            <div>
+                <Row type="flex" align="top" justify="start">
+                    <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+
                         <Form
                             labelCol={{ span: 4 }}
                             wrapperCol={{ span: 14 }}
                             layout="horizontal"
                         >
-                            <h1><center>User Details</center></h1>
-
                             <Row type="flex" align="top" justify="start">
-                            <Col xs ={24} sm={24} md={6} lg={6} xl={6}>
-                                <SideNavbar />
-                            </Col>
-                            <Col xs ={24} sm={24} md={18} lg={18} xl={18}>
-                            <Table
-                                    dataSource={data}
-                                    columns={columns}
+                                <Col xs={24} sm={24} md={6} lg={6} xl={6}>
+                                    <SideNavbar />
+                                </Col>
+                                <Col xs={24} sm={24} md={18} lg={18} xl={18}>
+                                <h1><center>User Details</center></h1>
+                                    <Table
+                                        dataSource={data}
+                                        columns={columns}
                                     />
 
-                            </Col>
-                            </Row>  
+                                </Col>
+                            </Row>
                         </Form>
-                    { /*
-                            <Descriptions>
-                                <Descriptions.Item label="UserName">abc</Descriptions.Item>
-                                <Descriptions.Item label="Telephone">1810000000</Descriptions.Item>
-                                <Descriptions.Item label="Live">Hangzhou, Zhejiang</Descriptions.Item>
-                                <Descriptions.Item label="Address">
-                                    No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
-                                </Descriptions.Item>
-                            </Descriptions>
-                            <Form.Item className="userCreateButton">
-                                <Button type="primary" a href="UpdateUser">Update </Button>
-    </Form.Item> --></Col>}
-                      
-                    </div> */}
-                </Col>
-            </Row>
+                    </Col>
+                </Row>
+            </div>
             <div>
                 <Footer />
             </div>
