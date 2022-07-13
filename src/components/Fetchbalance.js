e866e7e2f2178a27b8df0a83f745e8906ac96a68
gupta19chirag <gupta1chirag@gmail.com>
2022-07-13 11:25:59 +0530

    FetchBalance

--- src/components/Fetchbalance.js
+++ src/components/Fetchbalance.js
@@ -7,59 +7,98 @@ import {
   Form,
   Input,
   Button,
-  
+  Select
 } from "antd";
 import axios from "axios";
- var email;
- var phone;
- function setPhone(val){
-  phone = val.target.value;
-}
+import { Link, useNavigate } from "react-router-dom";
 
-function setEmail(val){
- email = val.target.value;
-}
+const { Option } = Select;
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
+
+function FetchBalance() {
+  const [form] = Form.useForm();
+  const navigate = useNavigate();
+  const onFinish = (values) => {
+    console.log(values);       
+    navigate('/Checkbalance',{state:{values}});
+  };
+
+  const onReset = () => {
+    form.resetFields();
+  };
+  const onCheck = () =>{
+
+  }
 
 
-function Fetchbalance() {
-  
-  
   return (
     <div className="createUser">
-        <div>
+      <div>
         <Navigation status={true} />
-        </div>
-        <div>
-            <SideNavbar />
-        </div>
-      
-    
-      <div className="userForm1"> 
-      
-     <Form
-          className="form1"
+      </div>
+      <div>
+        <SideNavbar />
+      </div>
+
+
+      <div className="userForm1">
+      <Form  className="form1"
           labelCol={{ span: 4 }}
-          wrapperCol={{ span: 14 }}
-        >        
-        <h1><center>Check Balance</center></h1>
-         <Form.Item label="Email" name="email" 
-          onChange={setEmail} >
-            <Input />
-          </Form.Item>      
-          <Form.Item label="Mobile No" name="phone" onChange={setPhone}>
-            <Input />
-          </Form.Item>      
-          <Form.Item className="userCreateButton">
-            <Button type="primary"  href="Checkbalance">Fetch </Button>
-          </Form.Item>
+          wrapperCol={{ span: 14 }}  form={form} name="control-hooks" onFinish={onFinish}>
+            <div><center><h1>Details</h1></center></div>
+            <br />  
+      <Form.Item
+        name="email"
+        label="Email"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="phone"
+        label="Phone Number"
+        rules={[
+          {
+            required: true,
+            
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item {...tailLayout}>      
+        <Button type="primary" htmlType="submit">
+          Fetch
+        </Button>
+        <Button htmlType="button" onClick={onReset}>
+          Reset
+        </Button>
         
-        </Form>
+      </Form.Item>
+    </Form>
       </div>
 
       <div>
-        <Footer /> 
-        </div>
+        <Footer />
       </div>
+    </div>
   );
 }
-export default Fetchbalance;
+export default FetchBalance;
